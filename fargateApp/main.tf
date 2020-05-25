06419317892f9cef340ca6ab5b3ae42f46e8ad33
岳人三牧 <gmimakiwithkamejiroandnana@gmail.com>
2020-05-25 22:40:48 +0900

    ElastiCache

--- fargateApp/main.tf
+++ fargateApp/main.tf
@@ -647,10 +647,51 @@ resource "aws_db_instance" "example" {
     }
 }
 
-module "mysql_dg" {
+module "mysql_sg" {
     source = "./security_group"
     name = "mysql-sg"
     vpc_id = aws_vpc.example.id
     port = 3306
+    cidr_blocks = [aws_vpc.example.cidr_block]
+}
+
+resource "aws_elasticache_parameter_group" "example" {
+    name = "example"
+    family = "redis5.0"
+
+    parameter {
+        name = "cluster-enabled"
+        value = "no"
+    }
+}
+
+resource "aws_elasticache_subnet_group" "example" {
+    name = "example"
+    subnet_ids = [aws_subnet.private_0.id, aws_subnet.private_1.id]
+}
+
+resource "aws_elasticache_replication_group" "example" {
+    replication_group_id = "example"
+    replication_group_description = "Cluster Disabled"
+    engine = "redis" # memcached or redis
+    engine_version = "5.0.4"
+    number_cache_clusters = 3 # プライマリノードとレプリカノードを足した数
+    node_type = "cache.m3.medium"
+    snapshot_window = "09:10-10:10"
+    snapshot_retention_limit = 7 # cacheとして保存するのでそんな長くなくていい
+    maintenance_window = "mon:10:40-mon:11:40"
+    automatic_failover_enabled = true # 自動フェイルオーバー
+    port = 6379
+    apply_immediately = false
+    security_group_ids = [module.redis_sg.security_group_id]
+    parameter_group_name = aws_elasticache_parameter_group.example.name
+    subnet_group_name = aws_elasticache_subnet_group.example.name
+}
+
+module "redis_sg" {
+    source = "./security_group"
+    name = "redis-sg"
+    vpc_id = aws_vpc.example.id
+    port = 6379
     cidr_blocks = [aws_vpc.example.cidr_block]
 }
