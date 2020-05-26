df4a7ba17ebc83532ec1013683fb73a6027fd4a0
岳人三牧 <gmimakiwithkamejiroandnana@gmail.com>
2020-05-26 23:12:35 +0900

    codebuild

--- fargateApp/main.tf
+++ fargateApp/main.tf
@@ -695,3 +695,156 @@ module "redis_sg" {
     port = 6379
     cidr_blocks = [aws_vpc.example.cidr_block]
 }
+
+resource "aws_ecs_repository" "example" {
+    name = "example"
+}
+
+resource "aws_ecr_lifecyncle_policy" "example" {
+    repository = aws_ecs_repository.example.name
+
+    policy = <<EOF
+    {
+        "rules": [
+            {
+                "rulePriority": 1,
+                "description": "Keep last 30 release tagged images",
+                "selection": {
+                    "tagStatus": "tagged",
+                    "tagPrefixList": ["release"],
+                    "countType": "imageCountMoreThan",
+                    "countNumber": 30,
+                },
+                "action": {
+                    "type": "expire"
+                }
+            }
+        ]
+    }
+EOF
+}
+
+data "aws_iam_policy_document" "codebuild" {
+    statement {
+        effect = "Allow"
+        resources = ["*"]
+
+        actions = [
+            "s3:PutObject",
+            "s3:GetObject",
+            "s3:GetObjectVersion",
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+            "ecr:GetAuthorizationToken",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:GetDownLoadUrlForLayer",
+            "ecr:GetRepositoryPolicy",
+            "ecr:DescribeRepositories",
+            "ecr:ListImages",
+            "ecr:DescribeImages",
+            "ecr:BatchGetImage",
+            "ecr:InitiateLayerUpload",
+            "ecr:UploadLayerPart",
+            "ecr:CompleteLayerUpload",
+            "ecr:PutImage",
+        ]
+    }
+}
+
+module "codebuild_role" {
+    source = "./iam_role"
+    name = "codebuild"
+    identifier = "codebuild.amazonaws.com"
+    policy = data.aws_iam_policy_document.codebuild.json
+}
+
+resource "aws_codebuild_project" "example" {
+    name = "example"
+    service_role = module.codebuild_role.iam_role_arn
+
+    # ソースとアーティファクトの両方をCODEPIPELINEと指定することで、CodePipeLineと連携することを宣言
+    source {
+        type = "CODEPIPELINE"
+    }
+
+    artifacts {
+        type = "CODEPIPELINE"
+    }
+
+    environment {
+        type = "LINUX_CONTAINER"
+        compute_type = "BUILD_GENERAL_SMALL"
+        image = "aws/codebuild/standard:2.0" # codebuild image
+        privileged_mode = true
+    }
+}
+
+data "aws_iam_policy_document" "codepipeline" {
+    statement {
+        effect = "Allow"
+        resources = ["*"]
+
+        actions = [
+            "s3:PutObject",
+            "s3:GetObject",
+            "s3:GetObjectVersion",
+            "s3:GetBucketVersioning",
+            "codebuild:BatchGetBuilds",
+            "codebuild:StartBuild",
+            "ecs:DescribeServices",
+            "ecs:DescribeTaskDefinition",
+            "ecs:DescribeTasks",
+            "ecs:ListTasks",
+            "ecs:RegisterTaskDefinition",
+            "ecs:UpdateService",
+            "iam:PassRole",
+        ]
+    }
+}
+
+module "codepipeline_role" {
+    source = "./iam_role"
+    name = "codepipeline"
+    identifier = "codepipeline.amazonaws.com"
+    policy = data.aws_iam_policy_document.codepipeline.json
+}
+
+resource "aws_s3_bucket" "artifact" {
+    bucket = "artifact-mimaki"
+
+    lifecycle_rule {
+        enabled = true
+
+        expiration {
+            days = "180"
+        }
+    }
+}
+
+resource "aws_codepipeline" "example" {
+    name = "example"
+    role_arn = module.codepipeline_role.iam_role_arn
+
+    stage {
+        name = "Source"
+
+        action {
+            name = "Source"
+            category = "Source"
+            owner = "ThirdParty"
+            provider = "Github"
+            version = 1
+            output_artifacts = ["Source"]
+
+            configuration = {
+                Owner = "YOUR_GITHUB_NAME"
+                Repo = "YOUR_GITHUB_REPOSITORY"
+                Branch = "master"
+                PollForSourceChanges = false
+            }
+        }
+    }
+
+    # TODO stageから
+}
