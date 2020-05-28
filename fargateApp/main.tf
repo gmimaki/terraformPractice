5109fcfd7339ac5eaccfb9220f0a34d0c12a5423
岳人三牧 <gmimakiwithkamejiroandnana@gmail.com>
2020-05-28 22:22:52 +0900

    codebuild

--- fargateApp/main.tf
+++ fargateApp/main.tf
@@ -841,10 +841,78 @@ resource "aws_codepipeline" "example" {
                 Owner = "YOUR_GITHUB_NAME"
                 Repo = "YOUR_GITHUB_REPOSITORY"
                 Branch = "master"
-                PollForSourceChanges = false
+                PollForSourceChanges = false # CodePipelineの起動はWebhookから行うため、ポーリングは無効にする
             }
         }
     }
 
-    # TODO stageから
+    stage {
+        name = "Build"
+        category = "Build"
+        owner = "AWS"
+        provider = "CodeBuild"
+        version = 1
+        input_artifacts = ["Source"]
+        output_artifacts = ["Build"]
+        configuration = {
+            ProjectName = aws_codebuild_project.example.id
+        }
+    }
+
+    stage {
+        name = "Deploy"
+
+        action {
+            name = "Deploy"
+            category = "Deploy"
+            owner = "AWS"
+            provider = "ECS"
+            version = 1
+            input_artifacts = ["Build"]
+
+            configuration = {
+                ClusterName = aws_ecs_cluster.example.name
+                ServiceName = aws_ecs_service.example.name
+                FileName = "imagedefinition.json" # buildspec.ymlの最後に作成しているJSONファイル
+            }
+        }
+    }
+
+    artifact_store {
+        location = aws_s3_bucket.artifact.id
+        type = "S3"
+    }
+}
+
+resource "aws_codepipeline_webhook" "example" {
+    name = "example"
+    target_pipeline = aws_codepipeline.example.name
+    target_action = "Source"
+    authentication = "GITHUB_HMAC"
+
+    authentication_configuration {
+        secret_token = "VeryRandomString"
+    }
+
+    filter {
+        json_path = "$.ref"
+        match_equals = "refs/heads/{Branch}"
+    }
+}
+
+provider "github" {
+    organization = "your-github-name"
+}
+
+resource "github_repository_webhook" "example" {
+    repository = "your-repository"
+
+    configuration {
+        url = aws_codepipeline_webhook.example.url
+        secret = "VeryRandomString" # aws_code_pupeline_webhook.exampleのsecretトークンと同じ値
+        content_type = "json"
+        insecure_ssl = false
+    }
+
+    events = ["push"]
 }
